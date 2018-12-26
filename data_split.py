88c783573b46651f771b363e5ed4d18fcb7377d4
ChenfeiXuHKU <chenfeixu@outlook.com>
2018-12-26 16:03:30 +0800

    update Logistic Regression

--- data_split.py
+++ data_split.py
@@ -9,8 +9,8 @@ import pandas as pd
 #import matplotlib.pyplot as plt
 import scipy
 import os
-#1. data process
-path = '../train_data/'
+
+path = './train_data/'
 
 name = 'train_data_5d_2017-12'
 data_set = pd.read_csv(path + name + '.csv')
@@ -26,33 +26,13 @@ test_set_t = data_set_t.drop_duplicates(keep=False)
 train_set = train_set_t.T
 test_set = test_set_t.T
 
-#train_y = train_set.iloc[-1:]
-#train_x = train_set.iloc[0:6]
-#
-#test_y = test_set.iloc[-1:]
-#test_x = test_set.iloc[0:6]
+tid = 1
 
-file_name = path + 'train_data_5d_2017-12_1'
+file_name = path + 'train_data_5d_2017-12_' + tid
 train_set.to_csv(file_name + '.csv', sep=',', na_rep='N/A', index=False)
 
-file_name = path + 'test_data_5d_2017-12_1'
+file_name = path + 'test_data_5d_2017-12_' + tid
 test_set.to_csv(file_name + '.csv', sep=',', na_rep='N/A', index=False)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
---
+++ utils.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 23 15:12:51 2018
+
+@author: chenf
+"""
+import numpy as np
+
+def sigmoid(z):
+
+    s = 1/(1 + 1/np.exp(z))
+
+    return s
+
+
